c20b12b41fdc40adff9c01ca3fc229494e067513
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-02-01 21:39:04 -0500

    updated genmarkdown and started questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,26 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const generateMarkdown = require('./util/generateMarkdown');
+const { title } = require('process');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+}, {
+    type: "input",
+    message: "What is the description of the project?",
+    name: "Description"
+}, {
+    type: "input",
+    message: "Table of Contents",
+    name: "Table of Contents"
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
